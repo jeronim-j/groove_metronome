d35182d6f3a9de3121340eb7718644ef74cdf04e
jeronim-j <jeronim.j@o2.pl>
2020-08-22 12:57:27 +0200

    Starting Logo Creation

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Metronome from "./Metronome";
+import Header from "./Header";
+
+
+const App = () => {
+    return (
+        <>
+            <Header/>
+            <Metronome/>
+        </>
+    )
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Header = () => {
+    return (
+        <div className="logo-container">
+            <h1 className="logo">GROOVE</h1>
+            <hr className="logo-underline"/>
+        </div>
+    )
+}
+
+export default Header;
