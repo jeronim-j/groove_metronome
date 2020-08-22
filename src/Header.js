d48da64ba94288829f131f59bb5a9de245f000ab
jeronim-j <jeronim.j@o2.pl>
2020-08-22 13:53:29 +0200

    Animated Logo

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import {ReactComponent as Logo} from "./assets/vinyl-record.svg";
 
 
 const Header = () => {
     return (
         <div className="logo-container">
-            <h1 className="logo">GROOVE</h1>
+            <h1 className="logo">GR<Logo className ="logo-vinyl"/>OVE</h1>
             <hr className="logo-underline"/>
         </div>
     )
