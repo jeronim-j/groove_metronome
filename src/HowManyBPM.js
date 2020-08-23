11733fc1f35cd716037001fffc088b674bcf1330
jeronim-j <jeronim.j@o2.pl>
2020-08-23 14:47:07 +0200

    Added avg bpm counter

--- src/HowManyBPM.js
+++ src/HowManyBPM.js
@@ -3,37 +3,48 @@ import * as Tone from "tone";
 
 const HowManyBPM = () => {
 
-
-
+    let beatArray = [];
+    let bpm;
 
     let setTime = () => {
+
         Tone.Transport.start(0);
         let time = Tone.now();
         let beatTime = Tone.Transport.seconds;
-        let bpm =  60 / beatTime;
+
+        if (beatArray.length < 4) {
+            beatArray.push(beatTime);
+        } else {
+            beatArray = [];
+            beatArray.push(beatTime);
+        }
+
+        let total = beatArray.reduce( function(acc, cur) {
+            return acc + cur
+        }, 0 )
+        let avg = total / beatArray.length;
+        bpm =  60 / avg;
+
 
         console.log(`audio ctx time: ` + time.toFixed(3));
         console.log(`transport clock: ` + beatTime.toFixed(3));
-        console.log(bpm);
-
-
+        console.log(`bpm: ` + bpm.toFixed(0));
         Tone.Transport.stop().start();
-
-
 
     }
 
 
 
+
+
     return (
-        <div className="how-many-bpm" style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+        <div className="how-many-bpm">
             <button className="metronome-button" onClick={setTime}>
                 Tap the beat!
             </button>
-            <div className="bpm-value">60bpm</div>
+            <div className="bpm-value">60BPM</div>
         </div>
     )
-
 }
 
 
